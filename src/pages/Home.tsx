197d676e5202ce0370a65fbcfaadfa5c95fd9f05
ocen <ocenjoe1991@gmail.com>
2026-06-26 01:59:36 +0100

    login, start free trial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -44,7 +44,7 @@ export default function Home() {
               className="bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-indigo-600 cursor-pointer border-none"
             >
               Start free trial
-            </a>
+            </button>
           </div>
         </nav>
 
